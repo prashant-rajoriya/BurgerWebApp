32abf91a546e75ff7d675aed8b8a8a8e9fb23b30
Solulab <solulab@Solulabs-MacBook-Pro.local>
2018-11-16 18:18:55 +0530

    Finish Project

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
+   
+  render () {
 
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => (
-      <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}</li>
-    ))
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => (
+        <li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+      ))
+  
+    return (
+      <Aux>
+          <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue with ckeckout? </p>
+        <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType='Success' clicked={this.props.purchaseContinue}>CONTINUE</Button>
+      </Aux>
+    )
+  }
 
-  return (
-    <Aux>
-        <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue with ckeckout? </p>
-      <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
-    </Aux>
-  )
 }
 
-export default orderSummary;  
+export default OrderSummary;  
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
+class Layout extends Component {
+
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => { 
+      return { showSideDrawer: !prevState.showSideDrawer};
+    })
+  }
+
+  render(){
     return (
       <Aux>
-        <Toolbar />
-        <div>
-          , SideBar, BackDrop
-        </div>
-        <main className={classes.Content}>{props.children}</main>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer 
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}/>
+        <main className={classes.Content}>{this.props.children}</main>
       </Aux>
         
     );
+  }
 }
 
-export default layout;
+export default Layout;
